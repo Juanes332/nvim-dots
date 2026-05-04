9222de6a05b8057f2ded6560f879284fb6626f4b
jenhz <bonillajuanes02@gmail.com>
2026-05-03 22:52:15 -0500

    feat: add Gentleman Kanagawa Blur theme (sakura default)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Default keymaps that are automatically set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Toggle between Gentleman Kanagawa Blur variants (sakura <-> blur)
+vim.keymap.set("n", "<leader>us", function()
+  vim.cmd("colorscheme gentleman-kanagawa-blur-sakura")
+  vim.notify("Gentleman Kanagawa Sakura loaded", vim.log.levels.INFO)
+end, { desc = "Colorscheme: Gentleman Kanagawa Sakura" })
+
+vim.keymap.set("n", "<leader>ub", function()
+  vim.cmd("colorscheme gentleman-kanagawa-blur")
+  vim.notify("Gentleman Kanagawa Blur loaded", vim.log.levels.INFO)
+end, { desc = "Colorscheme: Gentleman Kanagawa Blur" })
--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,12 +1,52 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- add gentleman-kanagawa-blur theme
+  {
+    "Gentleman-Programming/gentleman-kanagawa-blur",
+    name = "gentleman-kanagawa-blur",
+    priority = 1000,
+    opts = function()
+      require("gentleman_kanagawa_blur").setup({
+        variant = "sakura", -- default variant: sakura (also available: blur)
+        terminal_colors = true,
+        styles = {
+          comments = {},
+          keywords = {},
+          identifiers = {},
+          functions = {},
+          variables = {},
+          booleans = {},
+        },
+        integrations = {
+          alpha = true,
+          cmp = true,
+          flash = true,
+          gitsigns = true,
+          hop = false,
+          indent_blankline = true,
+          lazy = true,
+          lsp = true,
+          markdown = true,
+          mason = true,
+          navic = false,
+          neo_tree = false,
+          neogit = false,
+          neorg = false,
+          noice = true,
+          notify = true,
+          rainbow_delimiters = true,
+          telescope = true,
+          treesitter = true,
+        },
+        highlight_overrides = {},
+      })
+    end,
+  },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load gentleman-kanagawa-blur (sakura variant by default)
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "gentleman-kanagawa-blur-sakura",
     },
-  }
+  },
 }
